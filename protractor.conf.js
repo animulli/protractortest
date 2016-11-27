5858977c1c7c6280eedfb0d9dc5c105bbb5c7e14
animulli <lluc@illumina.com>
2016-11-26 22:41:29 -0800

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,10 +1,11 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['todo-spec.js'],
+  specs: ['*-spec.js'],
   multiCapabilities: [{
     'browserName': 'firefox'
   }, {
     'browserName': 'chrome',
-    'shardTestFiles': 'true'
+    shardTestFiles: true,
+    maxInstances: 2
   }]
 };
