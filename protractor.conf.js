5f6f9f2a2788a42770e3a921f73142098993d5ac
lluc3 <lluc3@ussd-ofcmc5278.local>
2016-11-26 23:58:17 -0800

    reporters

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,9 +3,43 @@ exports.config = {
   specs: ['*-spec.js'],
   multiCapabilities: [{
     'browserName': 'firefox'
-  }, {
-    'browserName': 'chrome',
-    shardTestFiles: true,
-    maxInstances: 2
-  }]
+  }
+  // ,{
+  //   'browserName': 'chrome',
+  //   shardTestFiles: true,
+  //   maxInstances: 2
+  // }
+  ],
+  framework: 'jasmine2',
+ //  onPrepare: function() {
+	//     var jasmineReporters = require('jasmine-reporters');
+	//     jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+	//         consolidateAll: true,
+	//         savePath: 'testresults',
+	//         filePrefix: 'xmloutput'
+	//     }));
+	// }
+
+	onPrepare: function() {
+	    var jasmineReporters = require('jasmine-reporters');
+
+	    // returning the promise makes protractor wait for the reporter config before executing tests
+	    return browser.getProcessedConfig().then(function(config) {
+	        // you could use other properties here if you want, such as platform and version
+	        var browserName = config.capabilities.browserName;
+
+	        var junitReporter = new jasmineReporters.JUnitXmlReporter({
+	            consolidateAll: true,
+	            savePath: 'testresults',
+	            // this will produce distinct xml files for each capability
+	            filePrefix: browserName + '-xmloutput',
+	            modifySuiteName: function(generatedSuiteName, suite) {
+	                // this will produce distinct suite names for each capability,
+	                // e.g. 'firefox.login tests' and 'chrome.login tests'
+	                return browserName + '.' + generatedSuiteName;
+	            }
+	        });
+	        jasmine.getEnv().addReporter(junitReporter);
+	    });
+	}
 };
